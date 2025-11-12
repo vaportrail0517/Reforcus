5b5f6ac3d875ceca86c564aeb69d665a28b06e8e
Ujiuzui <yujin124yujin@gmail.com>
2025-11-12 13:52:29 +0900

    Remove all files from repository

--- android/app/src/main/kotlin/com/example/reforcus/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.reforcus
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/constants.dart
+++
@@ -1,8 +0,0 @@
-class AppUrls {
-  static const youtubeShorts = 'https://m.youtube.com/shorts';
-  static const instagramReels = 'https://www.instagram.com/reels/';
-}
-
-class AllowedHosts {
-  static const set = {'m.youtube.com', 'www.youtube.com', 'www.instagram.com'};
-}
